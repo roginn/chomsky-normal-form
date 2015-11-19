efdead156d427935ec34a74e4dac9b3ede357aa3
Roger Garcia <roger@redealumni.com>
2015-11-18 23:51:01 -0200

    improved result steps visuals

--- app/controllers/grammars_controller.rb
+++ app/controllers/grammars_controller.rb
@@ -8,6 +8,16 @@ B -> b | bAb
   end
 
   def convert
+    @transformation_names = [
+      'Gramática inicial',
+      'Eliminação de recursão sobre S',
+      'Eliminação de regras com símbolos terminais não solitários',
+      'Eliminação de regras com mais de 2 símbolos não-terminais do lado direito',
+      'Eliminação de regras ε',
+      'Eliminação de produções unitárias',
+      'Eliminação de Símbolos Inúteis'
+    ]
+
     input_grammar = params[:grammar][:grammar]
     parser = GrammarParser.new(input_grammar)
     @grammar = parser.parse
